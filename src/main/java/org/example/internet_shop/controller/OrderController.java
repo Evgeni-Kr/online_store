1753ff222ece335c2e085e66922f24e4bd8980d3
Evgeni-Kr <krotevgeni1@gmail.com>
2025-12-02 19:52:35 +0300

    исправлены недочеты для перехода к работе над заказами

--- src/main/java/org/example/internet_shop/controller/OrderController.java
+++ src/main/java/org/example/internet_shop/controller/OrderController.java
@@ -1,15 +1,20 @@
 package org.example.internet_shop.controller;
 
+import org.example.internet_shop.Entity.Order;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
 
     @GetMapping("/getOrders")
-    public String getAllOrders(){
+    public String getAllOrders(Model model) {
+        List<Order> orders;
         return "orderPage";
     }
 }
